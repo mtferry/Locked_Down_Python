b72c89d6e0ea9436277b29cebdfaf1b7001df7a3
Mark F <ssboi@spikio.com>
2018-05-08 05:17:13 -0700

    add initial unit tests

--- Lib/lockdownlib.py
+++ Lib/lockdownlib.py
@@ -14,3 +14,39 @@ def lockdown():
     sys.modules[m].__file__ = None
     try: sys.modules[m].__spec__.origin = None
     except AttributeError: pass
+
+def unittest():
+  import os, io
+  lockdown()
+  passed = 0
+  blocked_cmds = [
+      'import sys',
+      'open("foo.txt","r")',
+      'io.open("foo.txt","r")',
+      'os.listdir(".")'
+    ]
+  allowed_cmds = [
+      'os.cpu_count()'
+    ]
+  for cmd in blocked_cmds:
+    try:
+      exec(cmd)
+      print("FAILED", cmd, "NO EXCEPTION RAISED")
+    except Exception as ex:
+      if type(ex) is RuntimeError and ex.args == ('lockdown is enabled',):
+        passed += 1
+        print('PASSED', cmd)
+      else:
+        print('FAILED', cmd, repr(ex))
+  for cmd in allowed_cmds:
+    try:
+      exec(cmd)
+      passed += 1
+      print('PASSED', cmd)
+    except Exception as ex:
+      print('FAILED', cmd, repr(ex))
+  print('\n', passed, 'of', len(blocked_cmds+allowed_cmds), 'tests passed')
+
+if __name__ == '__main__':
+  if len(sys.argv) > 1 and sys.argv[1] == 'unittest':
+    unittest()
